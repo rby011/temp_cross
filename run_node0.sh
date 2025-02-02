c7c6ac23253433f71bbb8a214ffe65be171d9036
changsun song <chsun.song@gmail.com>
2025-02-02 12:19:03 +0900

    Create run_node0.sh

---
+++ run_node0.sh
@@ -0,0 +1,12 @@
+
+# Node A(랭크=0) 실행
+export MASTER_ADDR=10.0.0.1
+export MASTER_PORT=12345
+
+accelerate launch \
+  --config_file ds_config_node0.yaml \
+  examples/scripts/sft_vlm.py \
+  --model_name_or_path ... \
+  --dataset_name ... \
+  --num_train_epochs 3 \
+  ...
